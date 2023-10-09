2c0c6769babe81b746c4c1bad25e5627e7cf584c
Ebrahimsamad <ebrahim7asn@gmail.com>
2023-10-09 09:29:54 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '6bb75ddf-6b27-4e78-94e7-08c623179340';
+export const MODAL_CLOSE_SEC = 2.5;
